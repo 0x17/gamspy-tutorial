83a08b4a24bc7010d3dba0bcec6bc51e91f779ac
André Schnabel <aschnabel@gams.com>
2024-07-25 16:01:51 +0200

    Minor restructuring

--- collect_data.py
+++ collect_data.py
@@ -9,6 +9,7 @@ home_address = 'Kågevägen 24, 93137 Skellefteå'
 near_threshold = 1.0  # in kilometers
 
 
+# as the crow flies distance in kilometers between two coordinates
 def haversine(lat1, lon1, lat2, lon2):
     r = 6371  # radius of the earth in kilometers
     phi1, lambda1 = math.radians(lat1), math.radians(lon1)
@@ -17,35 +18,23 @@ def haversine(lat1, lon1, lat2, lon2):
     delta_lambda = lambda2 - lambda1
     a = math.sin(delta_phi / 2) ** 2 + math.cos(phi1) * math.cos(phi2) * math.sin(delta_lambda / 2) ** 2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    # in kilometers
-    distance = r * c
-    return distance
+    return r * c
 
 
-resp = requests.get('https://koncept.orientering.se/api/v1/locations/104/checkpoints/?format=json')
-resp.encoding = 'utf-8'
-locations = resp.json()
-easy_locs = sorted([loc for loc in locations
-                    if loc['level'] == 10
-                    and loc['accessibility_adjusted']],
-                   key=lambda loc: loc['number'])
+def fetch_easy_locations():
+    resp = requests.get('https://koncept.orientering.se/api/v1/locations/104/checkpoints/?format=json')
+    resp.encoding = 'utf-8'
+    locations = resp.json()
+    easy_locs = sorted([loc for loc in locations
+                        if loc['level'] == 10
+                        and loc['accessibility_adjusted']],
+                       key=lambda loc: loc['number'])
+    return easy_locs
 
 
 def get_lat_long(address):
     data = requests.get(f'https://nominatim.openstreetmap.org/search?q={address}&format=json').json()
     return dict(lat=float(data[0]['lat']), lng=float(data[0]['lon'])) if data else None
-
-
-# my_home = dict(lat=64.75587868955067, lng=20.965543282936686)
-my_home = get_lat_long(home_address)
-
-
-def near(loc):
-    return haversine(my_home['lat'], my_home['lng'], loc['lat'], loc['lng']) <= near_threshold
-
-
-near_easy_locs = [loc for loc in easy_locs if near(loc)]
-print(len(near_easy_locs))
 
 
 def get_walking_distance(start_lat, start_lon, end_lat, end_lon):
@@ -59,16 +48,32 @@ def get_walking_distance(start_lat, start_lon, end_lat, end_lon):
         return None
 
 
-distances = [[loc1["number"], loc2["number"], get_walking_distance(loc1['lat'], loc1['lng'], loc2['lat'], loc2['lng'])]
-             for loc1 in near_easy_locs
-             for loc2 in near_easy_locs
-             if loc1 != loc2]
+def main():
+    # my_home = dict(lat=64.75587868955067, lng=20.965543282936686)
+    my_home = get_lat_long(home_address)
 
-obj = dict(locations=[loc['number'] for loc in near_easy_locs],
-           distances=distances,
-           descriptions=[[loc['number'], loc['short_description']] for loc in near_easy_locs],
-           coordinates=[[loc['number'], loc['lat'], loc['lng']] for loc in near_easy_locs])
+    def near(loc):
+        return haversine(my_home['lat'], my_home['lng'], loc['lat'], loc['lng']) <= near_threshold
 
-os.makedirs('scratch', exist_ok=True)
-with open('scratch/data.yaml', 'w', encoding='utf-8') as fp:
-    yaml.dump(obj, fp, default_flow_style=False, allow_unicode=True)
+    easy_locs = fetch_easy_locations()
+    near_easy_locs = [loc for loc in easy_locs if near(loc)]
+    print(len(near_easy_locs))
+
+    distances = [
+        [loc1["number"], loc2["number"], get_walking_distance(loc1['lat'], loc1['lng'], loc2['lat'], loc2['lng'])]
+        for loc1 in near_easy_locs
+        for loc2 in near_easy_locs
+        if loc1 != loc2]
+
+    obj = dict(locations=[loc['number'] for loc in near_easy_locs],
+               distances=distances,
+               descriptions=[[loc['number'], loc['short_description']] for loc in near_easy_locs],
+               coordinates=[[loc['number'], loc['lat'], loc['lng']] for loc in near_easy_locs])
+
+    os.makedirs('scratch', exist_ok=True)
+    with open('scratch/data.yaml', 'w', encoding='utf-8') as fp:
+        yaml.dump(obj, fp, default_flow_style=False, allow_unicode=True)
+
+
+if __name__ == '__main__':
+    main()
--- tsp-mtz.py
+++ tsp-mtz.py
@@ -5,60 +5,68 @@ import folium
 from gamspy import Container, Set, Alias, Equation, Sum, Model, Problem, Sense, Variable, Ord, Parameter, Card, \
     VariableType
 
-with open('scratch/data.yaml', encoding='utf-8') as fp:
-    data = yaml.safe_load(fp)
 
-m = Container()
-i = Set(m, 'i', description='locations', records=data['locations'])
-j = Alias(m, 'j', alias_with=i)
-d = Parameter(m, 'd', description='distance', domain=[i, j], records=data['distances'])
-x = Variable(m, name='x', domain=[i, j], type=VariableType.BINARY)
-u = Variable(m, name='u', domain=i, type=VariableType.POSITIVE)
+def solve_model(locations, distances):
+    m = Container()
+    i = Set(m, 'i', description='locations', records=locations)
+    j = Alias(m, 'j', alias_with=i)
+    d = Parameter(m, 'd', description='distance', domain=[i, j], records=distances)
+    x = Variable(m, name='x', domain=[i, j], type=VariableType.BINARY)
+    u = Variable(m, name='u', domain=i, type=VariableType.POSITIVE)
 
-leave_once = Equation(m, 'leave_once', domain=i)
-enter_once = Equation(m, 'enter_once', domain=j)
-mtz = Equation(m, 'mtz', domain=[i, j])
+    leave_once = Equation(m, 'leave_once', domain=i)
+    enter_once = Equation(m, 'enter_once', domain=j)
+    mtz = Equation(m, 'mtz', domain=[i, j])
 
-leave_once[i] = Sum(j.where[Ord(i) != Ord(j)], x[i, j]) == 1
-enter_once[j] = Sum(i.where[Ord(i) != Ord(j)], x[i, j]) == 1
-mtz[i, j].where[(Ord(i) != Ord(j)) & (Ord(i) > 1) & (Ord(j) > 1)] = u[i] - u[j] + Card(i) * x[i,j] <= Card(i) - 1
+    leave_once[i] = Sum(j.where[Ord(i) != Ord(j)], x[i, j]) == 1
+    enter_once[j] = Sum(i.where[Ord(i) != Ord(j)], x[i, j]) == 1
+    mtz[i, j].where[(Ord(i) != Ord(j)) & (Ord(i) > 1) & (Ord(j) > 1)] = u[i] - u[j] + Card(i) * x[i, j] <= Card(i) - 1
 
-tsp_mtz = Model(m, 'tsp_mtz',
-                equations=[leave_once, enter_once, mtz],
-                problem=Problem.MIP,
-                sense=Sense.MIN,
-                objective=Sum((i, j), d[i, j] * x[i, j]))
+    tsp_mtz = Model(m, 'tsp_mtz',
+                    equations=[leave_once, enter_once, mtz],
+                    problem=Problem.MIP,
+                    sense=Sense.MIN,
+                    objective=Sum((i, j), d[i, j] * x[i, j]))
 
-res = tsp_mtz.solve(output=sys.stdout)
-print(f'Optimal tour has length of approx. {round(tsp_mtz.objective_value/1000, 2)} km')
+    tsp_mtz.solve(output=sys.stdout)
+    print(f'Optimal tour has length of approx. {round(tsp_mtz.objective_value / 1000, 2)} km')
 
-arcs = []
-for rec in x.records.values.tolist():
-    if rec[2] > 0:
-        arcs.append((int(rec[0]), int(rec[1])))
-
-
-def succ(l):
-    return next(b for a, b in arcs if a == l)
+    arcs = []
+    for rec in x.records.values.tolist():
+        if rec[2] > 0:
+            arcs.append((int(rec[0]), int(rec[1])))
+    return arcs
 
 
-location_seq = [arcs[0][0]]
-cur = arcs[0][1]
-while cur != location_seq[0]:
-    location_seq.append(cur)
-    cur = succ(cur)
-location_seq.append(location_seq[0])
+def succ(arcs, loc_num):
+    return next(b for a, b in arcs if a == loc_num)
 
 
-def get_coords(l):
-    return next([lat, lng] for loc, lat, lng in data['coordinates'] if loc == l)
+def main():
+    with open('scratch/data.yaml', encoding='utf-8') as fp:
+        data = yaml.safe_load(fp)
+
+    arcs = solve_model(data['locations'], data['distances'])
+
+    location_seq = [arcs[0][0]]
+    cur = arcs[0][1]
+    while cur != location_seq[0]:
+        location_seq.append(cur)
+        cur = succ(arcs, cur)
+    location_seq.append(location_seq[0])
+
+    def get_coords(loc_num):
+        return next([lat, lng] for loc, lat, lng in data['coordinates'] if loc == loc_num)
+
+    route = [get_coords(loc_num) for loc_num in location_seq]
+
+    m = folium.Map(location=route[0], zoom_start=13)
+    for point in route:
+        folium.Marker(location=point).add_to(m)
+    folium.PolyLine(route, color="blue", weight=2.5, opacity=1).add_to(m)
+
+    m.save("scratch/hitta_tsp_solution.html")
 
 
-route = [get_coords(l) for l in location_seq]
-
-m = folium.Map(location=route[0], zoom_start=13)
-for point in route:
-    folium.Marker(location=point).add_to(m)
-folium.PolyLine(route, color="blue", weight=2.5, opacity=1).add_to(m)
-
-m.save("scratch/hitta_tsp_solution.html")
+if __name__ == '__main__':
+    main()
