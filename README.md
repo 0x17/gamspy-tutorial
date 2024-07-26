36c880df36d53534727865bfe92ae62ca70aa3f8
André Schnabel <aschnabel@gams.com>
2024-07-26 09:47:26 +0200

    Added very short GAMSPy model

---
+++ README.md
@@ -0,0 +1,11 @@
+# Collection of GAMSPy models for SOAK PhD day tutorial
+
+```
+pip install virtualenv
+virtualenv venv
+# Windows
+./venv/Scripts/activate
+# Linux
+source venv/bin/activate
+pip install -r requirements.txt
+```
---
+++ intro-examples/hellogamspy.py
@@ -0,0 +1,12 @@
+import sys
+from gamspy import Container, Variable, Equation, Model, Sense
+m = Container()
+x = Variable(m)
+e = Equation(m)
+e[...] = x == 23
+mod = Model(m, 'mymodel',
+            equations=[e],
+            sense=Sense.MIN,
+            objective=x)
+mod.solve(output=sys.stdout)
+print(f'Optimal = {x.toValue()}')
